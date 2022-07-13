d01c8b905f28bd609fc02f680ad8c04f90edfa1a
GonvoDS <86834763+GonvoDS@users.noreply.github.com>
2022-07-12 22:04:18 -0300

    Add files via upload

---
+++ RecuperatoriosTP/deSalvo.Gonzalo.2C.TP1/Tp1 Gonzalo de Salvo/Entidades/Operando.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        private double numero;
+
+        public string Numero
+        {
+            set 
+            {
+                this.numero = ValidarOperando(value);
+            }
+        }
+
+        public Operando()
+        {
+            numero = 0;
+        }
+        public Operando(double numero):this()
+        {
+            this.numero = numero;
+        }
+        public Operando(string strNumero):this()
+        {
+            Numero = strNumero;
+        }
+        private double ValidarOperando(string strNumero)
+        {
+            double numero;
+            if(double.TryParse(strNumero, out numero))
+            {
+                return numero;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        private static bool EsBinario(string binario)
+        {
+            foreach(char caracter in binario)
+            {
+                if(caracter != '0' && caracter != '1')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static string DecimalBinario(string numero)
+        {
+            double numeroEntero;
+            if(double.TryParse(numero, out numeroEntero))
+            {
+                return DecimalBinario(numeroEntero);
+            }
+            else
+            {
+                return "Valor invalido";
+            }
+
+            
+
+        }
+        public static string DecimalBinario(double numero)
+        {
+            string retorno = "";
+            if(numero < 0)
+            {
+                numero *= -1;
+            }
+            int numeroEntero = (int)numero;
+            int aux;
+            do
+            {
+                aux = numeroEntero%2;
+                numeroEntero/=2;
+                retorno += aux.ToString();
+            }while(numeroEntero > 0);
+            return retorno;
+        }
+        public static string BinarioDecimal(string binario)
+        {
+            if(EsBinario(binario))
+            {
+                char[] digitos = binario.ToCharArray();
+                digitos.Reverse();
+                int sum = 0;
+                for(int i = 0; i < digitos.Length; i++)
+                {
+                    if(digitos[i] == '1')
+                    {
+                        sum += (int)Math.Pow(2, i);
+                    }
+                }
+                return sum.ToString();
+            }
+            return "Valor invalido";
+        }
+        public static double operator +(Operando n1, Operando n2)
+        {
+            return n1.numero + n2.numero;
+        }
+        public static double operator -(Operando n1, Operando n2)
+        {
+            return n1.numero - n2.numero;
+        }
+        public static double operator *(Operando n1, Operando n2)
+        {
+            return n1.numero * n2.numero;
+        }
+        public static double operator /(Operando n1, Operando n2)
+        {
+            if(n2.numero != 0)
+            {
+                return n1.numero / n2.numero;
+            }
+            return double.MinValue;
+        }
+    }
+}
---
+++ RecuperatoriosTP/deSalvo.Gonzalo.2C.TP1/Tp1 Gonzalo de Salvo/MiCalculadora/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+        private void Limpiar()
+        {
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            lstOperaciones.Items.Clear();
+            cmbOperador.SelectedItem = null;
+            lblResultado.Text = "RESULTADO";
+        }
+        private double Operar(string numero1, string numero2, string operador)
+        {
+            Operando operando1 = new Operando(numero1);
+            Operando operando2 = new Operando(numero2);
+            char.TryParse(operador, out char miOperador);
+            return Calculadora.Operar(operando1, operando2, miOperador);
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            string operador = "";
+            bool operar = true;
+            if(string.IsNullOrEmpty(txtNumero1.Text) || string.IsNullOrEmpty(txtNumero2.Text) 
+                || double.TryParse(txtNumero1.Text,out double numero1)== false || double.TryParse(txtNumero2.Text,out double numero2)== false)     
+            {
+                MessageBox.Show("Los casilleros para los numeros se encuentran en blanco o ingresaron un valor invalido, por favor ingrese los numeros correspondientes", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                operar = false;
+            }
+            if(cmbOperador.SelectedItem is null)
+            {
+                cmbOperador.Text = "+";
+                operador = cmbOperador.Text;
+
+            }
+            else
+            {
+                operador = cmbOperador.Text;
+
+            }
+            if(operar)
+            {
+                btnConvertirABinario.Enabled = true;
+                btnConvertirADecimal.Enabled = false;
+                lblResultado.Text = Operar(txtNumero1.Text,txtNumero2.Text,operador).ToString();
+                lstOperaciones.Items.Add($"{txtNumero1.Text} {operador} {txtNumero2.Text} = {lblResultado.Text}");
+            }         
+                                     
+                        
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Desea cerrar la calculadora?", "Confirmacion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Dispose();
+            }
+        }
+        private void FormCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Desea cerrar la calculadora?", "Confirmacion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Dispose();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            string numeroConvertido = lblResultado.Text.ToString();
+            if(double.TryParse(numeroConvertido, out double resultado))
+            {
+            lblResultado.Text = Operando.DecimalBinario(resultado).ToString();
+            lstOperaciones.Items.Add($"{numeroConvertido} convertido a binario = {lblResultado.Text}");
+            btnConvertirABinario.Enabled = false;
+            btnConvertirADecimal.Enabled = true;       
+
+            }
+            else
+            {
+                MessageBox.Show("No es posible convertir el numero a binario", "Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            string numeroConvertido = lblResultado.Text.ToString();
+            lblResultado.Text = Operando.BinarioDecimal(numeroConvertido).ToString();
+            lstOperaciones.Items.Add($"{numeroConvertido} convertido a Decimal = {lblResultado.Text}");
+            btnConvertirABinario.Enabled = true;
+            btnConvertirADecimal.Enabled = false;
+
+
+        }
+    }
+}
